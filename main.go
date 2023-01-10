3f7350a0f9a78978008758a4bd9f8d4c80d63b5f
Dev Gill <26435947+sdevgill@users.noreply.github.com>
2023-01-10 11:30:00 +0000

    Final refactor

--- main.go
+++ main.go
@@ -9,6 +9,13 @@ import (
 	"sort"
 )
 
+const (
+	// InputFilePath - constant for input file path
+	InputFilePath = "./inputs/1.json"
+	// MegaBitMultiplier - constant for Megabits/second conversion
+	MegaBitMultiplier = 8
+)
+
 // DataPoint represents a single data point with a metric value and a timestamp
 type DataPoint struct {
 	MetricValue float64 `json:"metricValue"`
@@ -17,28 +24,28 @@ type DataPoint struct {
 
 // Convert to Megabits per second
 func convertToMegabits(metricValue float64) float64 {
-	megabits := (metricValue / 1000000) * 8
+	megabits := (metricValue / 1000000) * MegaBitMultiplier
 	return megabits
 }
 
 // Main function
 func main() {
 	// Read the input data from the file
-	data, err := readInput("./inputs/1.json")
+	inputData, err := readInput(InputFilePath)
 	if err != nil {
 		fmt.Println("Error reading input file:", err)
 		os.Exit(1)
 	}
 
 	// Sort the data by date
-	sortedData, err := sortData(data)
+	sortedInputData, err := sortData(inputData)
 	if err != nil {
 		fmt.Println("Error sorting data:", err)
 		os.Exit(1)
 	}
 
 	// Calculate the min, max, median and average values
-	min, max, median, avg, err := calculateStatistics(sortedData)
+	min, max, median, avg, err := calculateStatistics(sortedInputData)
 	if err != nil {
 		fmt.Println("Error calculating statistics:", err)
 		os.Exit(1)
@@ -51,13 +58,13 @@ func main() {
 	avgMegabits := convertToMegabits(avg)
 
 	// Print the results
-	printResults(sortedData, minMegabits, maxMegabits, medianMegabits, avgMegabits)
+	printResults(sortedInputData, minMegabits, maxMegabits, medianMegabits, avgMegabits)
 }
 
 // Helper functions
 
+// Read input from a file
 func readInput(inputPath string) ([]DataPoint, error) {
-	// Read the contents of the input file
 	bytes, err := ioutil.ReadFile(inputPath)
 	if err != nil {
 		return nil, err
@@ -72,21 +79,21 @@ func readInput(inputPath string) ([]DataPoint, error) {
 	return data, nil
 }
 
-func sortData(data []DataPoint) ([]DataPoint, error) {
-	// Sort the data by timestamps
-	sort.Slice(data, func(i, j int) bool { return data[i].Dtime < data[j].Dtime })
-	return data, nil
+// Sort the data by timestamps
+func sortData(inputData []DataPoint) ([]DataPoint, error) {
+	sort.Slice(inputData, func(i, j int) bool { return inputData[i].Dtime < inputData[j].Dtime })
+	return inputData, nil
 }
 
-func calculateStatistics(data []DataPoint) (float64, float64, float64, float64, error) {
+func calculateStatistics(inputData []DataPoint) (float64, float64, float64, float64, error) {
 	var sum float64
 
 	// Initialize the min and max values with the first data point
-	min := data[0].MetricValue
-	max := data[0].MetricValue
+	min := inputData[0].MetricValue
+	max := inputData[0].MetricValue
 
 	// Iterate through the data to find the min, max, and sum values
-	for _, datapoint := range data {
+	for _, datapoint := range inputData {
 		sum += datapoint.MetricValue
 		if datapoint.MetricValue < min {
 			min = datapoint.MetricValue
@@ -97,12 +104,12 @@ func calculateStatistics(data []DataPoint) (float64, float64, float64, float64, 
 	}
 
 	// Calculate the average value
-	n := len(data)
+	n := len(inputData)
 	avg := sum / float64(n)
 
 	// Create a slice of metric values and sort it
-	metricValues := make([]float64, len(data))
-	for i, datapoint := range data {
+	metricValues := make([]float64, len(inputData))
+	for i, datapoint := range inputData {
 		metricValues[i] = datapoint.MetricValue
 	}
 	sort.Float64s(metricValues)
